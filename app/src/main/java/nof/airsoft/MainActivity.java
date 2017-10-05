ba8674cd00cb2fa089a3bc3afad1c1296f8b5eb5
Henrique Couto <rick.couto@hotmail.com>
2017-10-05 15:24:17 -0300

    Fix Menu

--- app/src/main/java/nof/airsoft/MainActivity.java
+++ app/src/main/java/nof/airsoft/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -16,33 +18,44 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import nof.airsoft.Fragments.EquipesFragment;
+import nof.airsoft.Fragments.JogosMarcadosFragment;
+import nof.airsoft.Fragments.MinhaEquipeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-    private BottomNavigationView bottomNavigationView;
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            if (item.getItemId() ==  R.id.navigation_equipes) {
+                transaction.replace(R.id.content, new EquipesFragment()).addToBackStack(null).commit();
+                return true;
+            } else if (item.getItemId() == R.id.navigation_minha_equipe) {
+                transaction.replace(R.id.content, new MinhaEquipeFragment()).addToBackStack(null).commit();
+                return true;
+            } else if (item.getItemId() == R.id.navigation_jogos_marcados) {
+                transaction.replace(R.id.content, new JogosMarcadosFragment()).addToBackStack(null).commit();
+                return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() ==  R.id.navigation_equipes) {
-                } else if (item.getItemId() == R.id.navigation_minha_equipe) {
-                    startActivity(new Intent(getApplicationContext(), MinhaEquipeActivity.class));
-                } else if (item.getItemId() == R.id.navigation_perfil) {
-                }
-                return false;
-            }
-        });
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 
