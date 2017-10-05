8d48fbc56703491aafc2b91237a7c35874cc8695
DalmiroJunior <dalmirojr@gmail.com>
2017-10-05 14:26:06 -0300

    Activity nova

---
+++ app/src/main/java/nof/airsoft/MinhaEquipeActivity.java
@@ -0,0 +1,13 @@
+package nof.airsoft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinhaEquipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minha_equipe);
+    }
+}
